e7cf3fd97cca4a513667294e97a04ba5f976aa6b
Sage Belknap <sbelknap@cmc.com>
2025-11-05 12:34:22 -0700

    ModParticles added nether

---
+++ src/main/java/net/kaupenjoe/tutorialmod/init/ModParticles.java
@@ -0,0 +1,32 @@
+package biomesoplenty.init;
+
+import biomesoplenty.core.BiomesOPlenty;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.function.BiConsumer;
+
+public class ModParticles
+{
+    // Particle constants for Visceral Heap features only
+    public static final SimpleParticleType DRIPPING_BLOOD = new SimpleParticleType(false);
+    public static final SimpleParticleType FALLING_BLOOD = new SimpleParticleType(false);
+    public static final SimpleParticleType LANDING_BLOOD = new SimpleParticleType(false);
+    public static final SimpleParticleType PUS = new SimpleParticleType(false);
+
+    public static void registerParticles(BiConsumer<ResourceLocation, ParticleType<?>> func)
+    {
+        register(func, "dripping_blood", DRIPPING_BLOOD);
+        register(func, "falling_blood", FALLING_BLOOD);
+        register(func, "landing_blood", LANDING_BLOOD);
+        register(func, "pus", PUS);
+    }
+
+    private static <T extends ParticleType<? extends ParticleOptions>> T register(BiConsumer<ResourceLocation, ParticleType<?>> func, String name, T particle)
+    {
+        func.accept(new ResourceLocation(BiomesOPlenty.MOD_ID, name), particle);
+        return particle;
+    }
+}
