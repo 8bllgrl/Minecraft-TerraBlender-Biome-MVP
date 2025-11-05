8d4b2d50829b982b1f2068f2d5d3684abd59431a
Sage Belknap <sbelknap@cmc.com>
2025-11-05 09:41:19 -0700

    BOPFluids blood and flowing blood:

---
+++ src/main/java/net/kaupenjoe/tutorialmod/api/block/BOPFluids.java
@@ -0,0 +1,10 @@
+package biomesoplenty.api.block;
+
+import net.minecraft.world.level.material.FlowingFluid;
+import net.minecraft.world.level.material.Fluid;
+
+public class BOPFluids
+{
+    public static FlowingFluid FLOWING_BLOOD;
+    public static Fluid BLOOD;
+}
