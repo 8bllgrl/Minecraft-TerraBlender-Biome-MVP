15411ca8836383ac7e439c0c5a4ba293ea6729d8
Sage Belknap <sbelknap@cmc.com>
2025-11-05 09:45:24 -0700

    BOPSounds visceral heap

---
+++ src/main/java/net/kaupenjoe/tutorialmod/api/sound/BOPSounds.java
@@ -0,0 +1,12 @@
+package biomesoplenty.api.sound;
+
+import net.minecraft.core.Holder;
+import net.minecraft.sounds.SoundEvent;
+
+public class BOPSounds
+{
+	public static Holder<SoundEvent> MUSIC_BIOME_VISCERAL_HEAP;
+	public static SoundEvent BLOOD_AMBIENT;
+	public static SoundEvent FLESH_TENDON_DRIP;
+	public static SoundEvent PUS_BUBBLE_POP;
+}
