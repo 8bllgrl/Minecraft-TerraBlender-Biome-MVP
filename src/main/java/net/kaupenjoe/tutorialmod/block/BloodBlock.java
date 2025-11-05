12cd1962f22fcfc7870343694d377a0003c4873e
Sage Belknap <sbelknap@cmc.com>
2025-11-05 09:53:08 -0700

    BloodBlock block

---
+++ src/main/java/net/kaupenjoe/tutorialmod/block/BloodBlock.java
@@ -0,0 +1,14 @@
+package biomesoplenty.block;
+
+import net.minecraft.world.level.block.BucketPickup;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.FlowingFluid;
+
+public class BloodBlock extends LiquidBlock implements BucketPickup
+{
+    //is it required to have the field names like this? for compatibility with minecraft?
+    public BloodBlock(FlowingFluid p_54694_, BlockBehaviour.Properties p_54695_) {
+        super(p_54694_, p_54695_);
+    }
+}
